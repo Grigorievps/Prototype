01ea40ceeae6cf614ece60aa4f0cffc4cbe5e136
Grigoriev Pavel <p.s.grigoriev@yandex.ru>
2019-06-26 13:09:14 +0300

    Initial Commit

---
+++ Prototype/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Prototype
+//
+//  Created by Павел Григорьев on 26/06/2019.
+//  Copyright © 2019 GP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
